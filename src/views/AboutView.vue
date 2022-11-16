ac1ab7e3b57fe87b2151f8bd38f5a6fd99902027
Mika Hägerström <mikahagerstrom@hotmail.com>
2022-11-16 10:21:18 +0100

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2 class="myheader">Hellooooo booty</h2>
+    <div>
+      <button>Click me</button>
+      <p>Value: 1</p>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+
+  .myheader {
+    color: palevioletred;
+  }
+}
+</style>
---
+++ src/views/TodoView.vue
@@ -0,0 +1,34 @@
+<!-- eslint-disable prettier/prettier -->
+<script>
+import { createApp } from 'vue';
+
+createApp({
+    data() {
+        return {
+            count: 0
+        }
+    }
+}).mount('#count')
+</script>
+
+<template>
+  <div class="todo">
+    <h1>This is an todo page</h1>
+    <br />
+    <input placeholder="Write your todo" />
+    <div id="count">
+      <button @click="count++">Count is: {{ count }}</button>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .todo {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+</style>
