742691bfd89b00cdab3884ec6b67161e84f576ee
Mika Hägerström <mikahagerstrom@hotmail.com>
2022-11-16 12:09:58 +0100

    added header and button component

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'TaskHeader',
+    props: {
+        title: {
+            type: String,
+            required: true,
+    
+        },
+    },
+};
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: red;
+}
+</style>
--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,34 +1,28 @@
-<!-- eslint-disable prettier/prettier -->
-<script>
-import { createApp } from 'vue';
-
-createApp({
-    data() {
-        return {
-            count: 0
-        }
-    }
-}).mount('#count')
-</script>
-
 <template>
   <div class="todo">
-    <h1>This is an todo page</h1>
-    <br />
-    <input placeholder="Write your todo" />
-    <div id="count">
-      <button @click="count++">Count is: {{ count }}</button>
-    </div>
+   
+    <TaskHeader title="Task tracker"/>
   </div>
 </template>
+
+<script>
+import TaskHeader from '@/components/Header.vue';
+
+export default {
+  name: 'Todo-app',
+  components: {
+    TaskHeader,
+},
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
   .todo {
     min-height: 100vh;
     display: flex;
-    flex-direction: column;
     align-items: center;
+    border: solid white;
   }
 }
 </style>
