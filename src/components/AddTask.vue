cc8e6004e184c83374fd7b12078db188aff8e92c
Mika Hägerström <mikahagerstrom@hotmail.com>
2022-11-18 16:43:30 +0100

    added bsc style to form

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -10,14 +10,71 @@
         </div>
         <div class="form-control form-control-check">
             <label for="">Set reminder</label>
-            <input type="checkbox" name="reminder"/>
+            <input type="checkbox" name="reminder" />
         </div>
-        <input type="submit" value="Save task" class="baseButton"/>
+        <input type="submit" value="Save task" class="blockBtn" />
     </form>
 </template>
 
 <script>
 export default {
     name: 'AddTask',
+    data() {
+        return {
+            text: '',
+            day: '',
+            reminder: false,
+        }
+    }
 }
 </script>
+
+<style>
+.add-form {
+    margin-bottom: 40px;
+}
+.blockBtn {
+    width: 100%;
+    border-radius: 5px;
+    padding: 10px 15px;
+    background-color: rgb(58, 56, 56);
+    color: white;
+    font-weight: 300;
+}
+
+.form-control {
+    margin: 20px 0;
+}
+
+.form-control label {
+    display: block;
+}
+
+.form-control input {
+    width: 100%;
+    height: 40px;
+    margin: 5px;
+    padding: 3px 7px;
+    font-size: 17px;
+
+}
+
+.form-control-check {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.form-control-check label {
+    flex: 1;
+}
+
+.form-control-check input {
+    flex: 2;
+    height: 20px;
+}
+
+
+
+</style>
+
