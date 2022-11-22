0e3ba545b11ec67009a0a36c6fad015e33b6febe
Mika Hägerström <mikahagerstrom@hotmail.com>
2022-11-22 14:44:46 +0100

    can now add task to json server

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -33,8 +33,16 @@ export default {
     toggleAddTask() {
       this.showAddTask = !this.showAddTask;
     },
-    addTask(task) {
-      this.tasks = [...this.tasks, task]
+    async addTask(task) {
+      const res = await fetch(`${url}/tasks`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(task),
+      })
+      const data = await res.json()
+      this.tasks = [...this.tasks, data]
     },
     deleteTask(id) {
       if (confirm('Are you sure you want to delete task?')) {
